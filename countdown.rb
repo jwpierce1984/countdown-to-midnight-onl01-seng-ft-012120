66e8634dd3743abb8754570dcaef710bd2cb4b56
Justin Pierce <jwpierce1984@gmail.com>
2020-01-31 14:30:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,12 +3,8 @@
 def countdown(number)
   until number == 0
     puts "#{number} SECOND(S)!"
-    number -= 1
     countdown_with_sleep
+    number -= 1
   end
 puts "HAPPY NEW YEAR!"  
-end
-
-def countdown_with_sleep (seconds)
-  sleep(seconds)
 end
